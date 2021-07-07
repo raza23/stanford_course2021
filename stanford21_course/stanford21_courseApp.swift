cc4ec4a6e5066556cff3d1fefdce977689e39037
Raza <rtshareef@gmail.com>
2021-07-07 17:35:03 -0400

    first commit

---
+++ stanford21_course/stanford21_courseApp.swift
@@ -0,0 +1,17 @@
+//
+//  stanford21_courseApp.swift
+//  stanford21_course
+//
+//  Created by Raza Shareef on 7/7/21.
+//
+
+import SwiftUI
+
+@main
+struct stanford21_courseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
